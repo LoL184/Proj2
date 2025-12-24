6d0c66bea66d9438b40b52fed1fbc9557aeb7eb0
Manya <maks01021980a@gmail.com>
2025-12-24 20:52:18 +0300

    Done task 1

--- src/myproject/app.py
+++ src/myproject/app.py
@@ -1,9 +1,11 @@
 from utils import greet, fetch_status
+from utils import day_advice
 
 
 def main():
-    print(greet("Kiwi"))
-    fetch_status("https://example.com")
+    # print(greet("Kiwi"))
+    # fetch_status("https://example.com")
+    print(day_advice()['advice'])
 
 
 if __name__ == "__main__":
--- src/myproject/utils.py
+++ src/myproject/utils.py
@@ -1,4 +1,5 @@
 import requests
+from fastapi import FastAPI
 
 
 def greet(name: str) -> str:
@@ -7,3 +8,9 @@ def greet(name: str) -> str:
 
 def fetch_status(url: str) -> str:
     return str(requests.get(url, timeout=5))
+
+app = FastAPI(title="Advice wrapper")
+
+@app.get("/advice")
+def day_advice():
+    return {"advice": requests.get("https://api.adviceslip.com/advice", timeout=5).json()["slip"]["advice"]}
