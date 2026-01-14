7ee3f90bdf93b724c10f3e41dd30356a8a3b2fab
Manya <maks01021980a@gmail.com>
2026-01-14 20:46:08 +0300

    Lesosn 14

--- src/myproject/app.py
+++ src/myproject/app.py
@@ -1,11 +1,15 @@
 from utils import greet, fetch_status
 from utils import day_advice
+import utils
 
 
 def main():
     # print(greet("Kiwi"))
     # fetch_status("https://example.com")
-    print(day_advice()['advice'])
+    # print(day_advice())      # lesson 14 task 1
+    # print(*utils.session_day_advices(), sep='\n')  # lesson 14 task 2
+    # print(utils.get_random_dog_url())  # lesson 14 task 3
+    print(utils.wrong_one())
 
 
 if __name__ == "__main__":
--- src/myproject/utils.py
+++ src/myproject/utils.py
@@ -9,8 +9,34 @@ def greet(name: str) -> str:
 def fetch_status(url: str) -> str:
     return str(requests.get(url, timeout=5))
 
+
+def wrong_one():
+    r = requests.get(url="https://jsonplaceholder.typicode.com/xyz", timeout=5)
+    return f"Error {r.status_code}"
+
 app = FastAPI(title="Advice wrapper")
 
 @app.get("/advice")
 def day_advice():
-    return {"advice": requests.get("https://api.adviceslip.com/advice", timeout=5).json()["slip"]["advice"]}
+    try:
+        return requests.get("https://api.adviceslip.com/advice", timeout=5).json()["slip"]["advice"]
+    except ValueError:
+        return "Error Unable to connect."
+
+
+@app.get("/ses_advice")
+def session_day_advices():
+    with requests.Session() as session:
+        advices = []
+        for i in range(3):
+            r = session.get("https://api.adviceslip.com/advice", timeout=5)
+            r.raise_for_status()
+            advices.append(r.json()['slip']["advice"])
+        return tuple(advices)
+
+
+@app.get("/random_dog_url")
+def get_random_dog_url():
+    r = requests.get(url="https://dog.ceo/api/breeds/image/random", timeout=5)
+    r.raise_for_status
+    return r.json()["message"]
